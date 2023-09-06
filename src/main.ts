e2f507de29dcf43afc9b2a4cd6e75356b0fff6e7
Tapas <tapasnayak74@gmail.com>
2023-09-06 11:26:43 +0530

    Swagger docuuement updated

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Todo App')
+    .setDescription('Todo app backend')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('todo')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
